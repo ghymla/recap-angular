20895b9e4286ecafc1be4ce9ab93bc0a0424fa9a
cloteau camille <camille.cloteau@gmail.com>
2022-08-13 16:41:51 +0200

    add module en component html

---
+++ src/app/html/html/html.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-html',
+  templateUrl: './html.component.html',
+  styles: [
+  ]
+})
+export class HtmlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
