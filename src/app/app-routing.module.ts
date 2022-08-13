eec4c992791207671d77718dacc85b00130e6ddd
cloteau camille <camille.cloteau@gmail.com>
2022-08-13 18:03:24 +0200

    start doing html page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNoFoundComponent } from "./page-no-found/page-no-found.component";
+import { HtmlComponent } from "./html/html/html.component";
 
 const routes: Routes = [
+  {path: 'html', component: HtmlComponent },
   {path: '**', component: PageNoFoundComponent}
 ];
 
--- src/app/html/html/html.component.ts
+++ src/app/html/html/html.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { reduce } from 'rxjs';
 
 @Component({
   selector: 'app-html',
   templateUrl: './html.component.html',
   styles: [
+    'code { font-weight: gold; }'
   ]
 })
 export class HtmlComponent implements OnInit {
