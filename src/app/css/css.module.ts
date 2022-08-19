d31157e3c68e296c402a5004eb67938588896207
cloteau camille <camille.cloteau@gmail.com>
2022-08-19 16:15:11 +0200

    add css module/component/service

---
+++ src/app/css/css.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CssComponent } from './css/css.component';
+
+
+
+@NgModule({
+  declarations: [
+    CssComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CssModule { }
